b1526151107070314e736f07072e7e6b1585d56a
ErbaZZ <weerawat.paw@gmail.com>
2018-07-23 10:54:03 +0200

    [Fix] inputDimension parameter

--- app/src/main/java/com/example/student/tensorflowmobiledemo/TFPredictor.java
+++ app/src/main/java/com/example/student/tensorflowmobiledemo/TFPredictor.java
@@ -8,20 +8,19 @@ public class TFPredictor {
     private static String modelFile;
     private static String inputNode;
     private static String outputNode;
-    private static long[] inputDimension;
     private static TensorFlowInferenceInterface tfInterface;
     private AssetManager assets;
 
-    public TFPredictor(String model, String input, String output, long[] inputDim, AssetManager assets) {
+    public TFPredictor(String model, String input, String output, AssetManager assets) {
         this.modelFile = model;
         this.inputNode = input;
         this.outputNode = output;
-        this.inputDimension = inputDim;
         this.assets = assets;
         tfInterface = new TensorFlowInferenceInterface(assets, modelFile);
     }
 
-    public float[] predict(float[] input) {
+    public float[] predict(float[] input, long[] inputDim) {
+        long[] inputDimension = inputDim;
         tfInterface.feed(inputNode, input, inputDimension);
         tfInterface.run(new String[] {outputNode});
         float[] result = new float[(int)inputDimension[0]];
