7f74392a5c4b95de436d1da29b195e35db41160b
MrWhiteC <woramethr@gmail.com>
2018-07-24 15:11:42 +0700

    Implement Battery Receiver in Main Activity

--- app/src/main/java/com/example/student/tensorflowmobiledemo/MainActivity.java
+++ app/src/main/java/com/example/student/tensorflowmobiledemo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.student.tensorflowmobiledemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView battery;
+    private BroadcastReceiver aBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+            battery.setText(String.valueOf(level)+"%");
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        battery = (TextView)this.findViewById(R.id.text1);
+        this.registerReceiver(this.aBatInfoReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 }
